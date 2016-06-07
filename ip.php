2ec9860572ae323af0bf567a886cee4f4d1b38c2
Alec Gorge <alecgorge@gmail.com>
2016-06-07 17:43:35 -0400

    final fix

--- ip.php
+++ ip.php
@@ -4,8 +4,8 @@ if(array_key_exists('dbug', $_GET)) {
 	var_dump($_SERVER);
 }
 
-if(array_key_exists('X-Forwarded-For', $_SERVER)) {
-	$parts = explode(',', $_SERVER['X-Forwarded-For']);
+if(array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+	$parts = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
 	echo $parts[0] . "\n";
 }
 else {
